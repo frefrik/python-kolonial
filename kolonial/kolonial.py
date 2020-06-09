bb5cdd3e6657b744de994a9ae756d9c83a6f88fb
frefrik <fredrik@haarstad.no>
2020-06-09 08:36:29 +0200

    Add docstrings

--- kolonial/kolonial.py
+++ kolonial/kolonial.py
@@ -7,6 +7,10 @@ from http.cookiejar import MozillaCookieJar
 logger = logging.getLogger(__name__)
 
 class Kolonial(object):
+    """
+    Class for accessing Kolonial.no API.
+
+    """
     cookiesFile = 'kolonial_cj.txt'
     cj = MozillaCookieJar(cookiesFile)
 
@@ -68,20 +72,28 @@ class Kolonial(object):
             return None
 
     def _get(self, endpoint):
-        
         return self._internal_call('GET', endpoint)
 
     def _post(self, endpoint, payload=None):
-        
         return self._internal_call('POST', endpoint, payload)
 
     def product_categories(self):
+        """
+        Gets list of all product categories.
+
+        """
         results = self._get('productcategories/')
         
         return results
 
     def product_category(self, product_category_id=None):
+        """
+        Gets all products in a given product category.
 
+        Parameters:
+            - product_category_id - the id of the product category
+        
+        """
         if product_category_id:
             results = self._get('productcategories/%s/' % (product_category_id))
         else:
@@ -90,6 +102,13 @@ class Kolonial(object):
         return results
 
     def product(self, product_id=None):
+        """
+        Gets extended information about specified product.
+
+        Parameters:
+            - product_id - the id of the product
+        
+        """
         if product_id:
             results = self._get('products/%s/' % (product_id))
         else:
@@ -98,11 +117,25 @@ class Kolonial(object):
         return results
 
     def search(self, query=None):
+        """
+        Search for a product. Can be used for both text queries and numeric barcodes
+
+        Parameters:
+            - query - the search query
+        
+        """
         results = self._get('search/?q=%s' % (query))
 
         return results
 
     def search_recipe(self, query=None):
+        """
+        Search for a recipe.
+
+        Parameters:
+            - query - the search query
+        
+        """
         if query:
             results = self._get('search/recipes/?q=%s' % (query))
         else:
@@ -111,21 +144,43 @@ class Kolonial(object):
         return results
 
     def recipe_tags(self):
+        """
+        Gets list of recipe tags.
+
+        """
         results = self._get('recipe-tags/')
 
         return results
 
     def recipe_tag(self, recipe_tag_id):
+        """
+        Gets all recipes in specified recipe tag.
+
+        Parameters:
+            - recipe_tag_id - the id of the recipe tag
+        
+        """
         results = self._get('recipe-tags/%s/' % (recipe_tag_id))
 
         return results
 
     def recipe_plans(self):
+        """
+        Gets list of your suggested recipe plans.
+
+        """
         results = self._get('recipes/plans/current/')
 
         return results
 
     def recipe(self, recipe_id=None):
+        """
+        Gets extended information about specified recipe, including product details.
+
+        Parameters:
+            - recipe_id - the id of the recipe
+        
+        """
         if recipe_id:
             results = self._get('recipes/%s/' % (recipe_id))
         else:
@@ -134,16 +189,31 @@ class Kolonial(object):
         return results
 
     def recipes_likes(self):
+        """
+        Gets list of your liked recipes.
+
+        """
         results = self._get('recipes/likes/')
 
         return results
 
     def recipes_purchased(self):
+        """
+        Gets list of your liked recipes.
+
+        """
         results = self._get('recipes/purchased/')
 
         return results
 
     def recipe_like(self, recipe_id=None):
+        """
+        Toggles like button on a specified recipe.
+
+        Parameters:
+            - recipe_id - the id of the recipe
+        
+        """
         if recipe_id:
             results = self._post('recipes/%s/like-toggle/' % (recipe_id))
         else:
@@ -152,6 +222,10 @@ class Kolonial(object):
         return results
 
     def cart(self):
+        """
+        View the contents of your cart.
+
+        """
         results = self._get('cart/')
 
         return results
@@ -182,6 +256,10 @@ class Kolonial(object):
         return results
 
     def clear_cart(self):
+        """
+        Clear the contents of your cart.
+
+        """
         results = self._post('cart/clear/')
 
         return results
